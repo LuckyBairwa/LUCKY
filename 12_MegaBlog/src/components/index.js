52cc351f93611ead324e5abca123c8302920538b
LUCKY BAIRWA <luckyshairwal@gmail.com>
2024-01-20 17:32:50 +0530

    Lecture 25 cahnges

--- 12_MegaBlog/src/components/index.js
+++ 12_MegaBlog/src/components/index.js
@@ -3,6 +3,7 @@ import Container from './container/Container'
 import Footer from "./Footer/Footer";  
 import Logo from "./Logo"; 
 import LogoutBtn from "./Header/LogoutBtn"; 
+import RTE from "./RTE";
 
 
 export {
@@ -10,5 +11,6 @@ export {
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
